8dcf51e7b1dbb92c88eba9ae55a0391923973a67
0xmhc <mohcinejawad783@gmail.com>
2025-01-26 16:14:50 +0100

    create first POST Api

--- server/server.js
+++ server/server.js
@@ -7,3 +7,16 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const notes = [];
+
+app.post("/notes", (req, res) => {
+  const data = req.body();
+  console.log(`Body: ${body}`);
+  notes.push(data.title);
+  res.send(true);
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server has started on port ${port}`);
+});
